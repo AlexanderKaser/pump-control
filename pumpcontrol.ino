972ec47e17f9cd8804dd85851801ea827ba0abfc
AlexanderKaser <62385010+AlexanderKaser@users.noreply.github.com>
2022-08-04 14:51:54 +0200

    Update pumpcontrol.ino
    function switch_pump changed
    non_blocking_timed_pin added
    ventil wokflow changed

--- pumpcontrol.ino
+++ pumpcontrol.ino
@@ -1,3 +1,41 @@
+class non_blocking_timed_pin
+{
+	public:
+      non_blocking_timed_pin()
+    	{}
+        non_blocking_timed_pin(int pin, unsigned long time_span, string name): pin_(pin),time_span_(time_span), name_(name)
+        {
+        }
+    	void digital_write_timed(unsigned int state_start, unsigned int state_end)
+        {
+        	if(running_)
+          {
+             	unsigned long diff = millis() -timer_;
+            	if(diff < time_span_){
+                	return;
+              }
+              digitalWrite(pin_, state_end);
+            	Serial.print(name_);
+              Serial.print(name_);
+              Serial.println(state_start);
+              running_ = false;
+              return;
+          }
+          digitalWrite(pin_, state_start);
+          Serial.print(name_);
+          Serial.println(state_start);
+          timer_ = std::chrono::system_clock::now();
+          running_ = true;
+        };
+	private:
+    	  unsigned long timer_;
+        bool running_ = false;
+        
+        unsigned int pin_;
+        unsigned long time_span_;
+        std::string name_;
+};
+
 const int TRIGGER_FISH_PIN = 2;
 const int ECHO_FISH_PIN = 3;
 const int TRIGGER_BARREL_PIN = 4;
@@ -27,6 +65,11 @@ long last_distance_barrel = 0;
 long distance_fish = 0;
 long last_distance_fish = 0;
 
+bool start_ventil_1_=true,start_ventil_2=false,start_ventil_3=false;
+non_blocking_timed_pin ventil1_;
+non_blocking_timed_pin ventil2_;
+non_blocking_timed_pin ventil3_;
+
 void setup() {
 
   Serial.begin(9600);
@@ -47,6 +90,11 @@ void setup() {
   digitalWrite(VALVE_3_PIN, HIGH);
 
   Serial.println("booting complete");
+  
+ ventil1_ = non_blocking_timed_pin(VALVE_1_PIN,VALVE_1_ON,"ventil 1");
+ ventil2_ = non_blocking_timed_pin(VALVE_2_PIN,VALVE_2_ON,"ventil 2");
+ ventil3_ = non_blocking_timed_pin(VALVE_2_PIN,VALVE_3_ON,"ventil 3");
+  
   delay(1000);
 }
 
@@ -64,7 +112,7 @@ long get_Distance_Fish() {
   distance = ((duration / 2.) * 0.03432) + 0, 5;
   return distance;
 }
-
+  
 long get_Distance_Barrel() {
   long duration = 0;
   long distance = 0;
@@ -96,42 +144,20 @@ void loop() {
   long distance_fish = get_Distance_Fish();
   long distance_barrel = get_Distance_Barrel();
 
-  if (distance_barrel < 12) { //die 12 unbedingt überprüfen sonst zieht es aus Versehen die ganze Zeit Strom
-    int state_valve_1 = digitalRead(VALVE_1_PIN);
-    int state_valve_2 = digitalRead(VALVE_2_PIN);
-    int state_valve_3 = digitalRead(VALVE_3_PIN);
-
-    if ((state_valve_1 == HIGH) && (state_valve_2 == HIGH) && (state_valve_3 == HIGH)) { // wenn alle Ventile bis dato ausgeschaltet sind
-      // wir fangen beim ersten Ventil an, lassen es ein bis Timer abgelaufen und schalten dann zwei und drei
-      digitalWrite(VALVE_1_PIN, LOW);
-      valve_1_timer_ = millis();
-      Serial.println("Ventil 1 ein");
+  if (distance_fish < 12) { //die 12 unbedingt überprüfen sonst zieht es aus Versehen die ganze Zeit Strom
+    if(start1)
+    {
+    	start2 = ventil1.digital_write_timed(HIGH,LOW);
+      start1 = !start2;
+    }else if(start2)
+    {
+    	start3 = ventil2.digital_write_timed(HIGH,LOW);
+      start2 = !start3;
+    }else if(start3)
+    {
+    	start1 = ventil3.digital_write_timed(HIGH,LOW);
+      start3 = !start1;
     }
-    if (state_valve_1 == LOW) {
-      if (time_span_v1 > VALVE_1_ON) { // Von Ventil 1 ist die Zeit abgelaufen
-        digitalWrite(VALVE_1_PIN, HIGH);
-        digitalWrite(VALVE_2_PIN, LOW);
-        valve_2_timer_ = millis();
-        Serial.println("Ventil 1 aus");
-        Serial.println("Ventil 2 ein");
-      }
-    }
-    if (state_valve_2 == LOW) {
-      if (time_span_v2 > VALVE_2_ON) { //Von Ventil 2 ist die Zeit abgelaufen
-        digitalWrite(VALVE_2_PIN, HIGH);
-        digitalWrite(VALVE_3_PIN, LOW);
-        valve_3_timer_ = millis();
-        Serial.println("Ventil 2 aus");
-        Serial.println("Ventil 3 ein");
-      }
-    }
-    if (state_valve_3 == LOW) {
-      if (time_span_v3 > VALVE_3_ON) {
-        digitalWrite(VALVE_3_PIN, HIGH);
-        Serial.println("Ventil 3 aus");
-      }
-    }
-
 
     if ((distance_barrel > 45) || (distance_fish < 8)) {
       return;
@@ -158,22 +184,25 @@ void loop() {
 }
 
 void switch_pump() {
-    unsigned long time_span_pump = millis() - pump_timer_;
+  unsigned long time_span_pump = millis() - pump_timer_;
+  if(time_span_pump <= PUMP_MAIN_ON){
+    return;
+  }
   int pump_state = digitalRead(PUMP_MAIN_PIN);
   Serial.print("Pumpenstatus: ");
   Serial.println(pump_state);
-
-  if ((pump_state == HIGH) && (time_span_pump > PUMP_MAIN_ON)) {
+ 
+    
+  if (pump_state == HIGH) {
     digitalWrite(PUMP_MAIN_PIN, LOW);
-    pump_timer_ = millis();
     Serial.println("Pumpe ein");
   }
-  if ((pump_state == LOW) && (time_span_pump > PUMP_MAIN_ON)) {
+  if (pump_state == LOW) {
     // Pumpe lang genug gelaufen, ausschalten
     // rein nur zur Sicherheit auch eine Minute ausgeschaltet lassen
     // dafür verwenden wir den pump_timer_ noch einmal
-
     digitalWrite(PUMP_MAIN_PIN, HIGH);
-    pump_timer_ = millis();
+    Serial.println("Pumpe aus");
   }
+  pump_timer_ = millis();
 }
